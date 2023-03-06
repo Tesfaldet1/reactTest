ed2cbdc4ec35b5b3f6d500eda00d339a29a3bc45
Tesfaldet Weldemicheal <tweldemicheal@gmail.com>
2023-03-06 21:08:56 +0100

    additional change

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,14 +1,16 @@
-import "./EspenseItem.css";
+import ExpenseDate from "./ExpeseDate";
+import Card from "./Card";
+import "./ExpenseItem.css";
 
 function EspenseItem(props) {
   return (
-    <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
-      <div expense-item_description>
+    <Card className="expense-item">
+      <ExpenseDate date={props.date} />
+      <div className="expense-item_description">
         <h2>{props.title}</h2>
         <div className="expense-item_price">${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 export default EspenseItem;
