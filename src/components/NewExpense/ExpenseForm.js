c1696762f413d1eb0b899adc3886f6909ffb1250
Tesfaldet Weldemicheal <tweldemicheal@gmail.com>
2023-03-08 13:44:34 +0100

    addtional change

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,4 +1,6 @@
-import React from "react";
+//import React from "react";
+import React, { useState } from "react";
+
 import "./ExpenseForm.css";
 
 /**
@@ -8,11 +10,61 @@ import "./ExpenseForm.css";
  **/
 
 const ExpenseForm = (props) => {
-  const titleChangHandler = () => {
-    console.log("title changed!");
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredAmount, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+
+  // const [userInput, setUserInput] = useState({
+  // enteredTitle: "",
+  // enteredAmount: "",
+  // enteredDate: "",
+  // });
+
+  const titleChangHandler = (event) => {
+    setEnteredTitle(event.target.value);
+    /* setUserInput({
+      ...userInput,
+      enteredTitle: event.target.value,
+    });
+  */
+  };
+
+  const amountChangeHandler = (event) => {
+    setEnteredAmount(event.target.value);
+    /*setUserInput({
+        ...userInput,
+        enteredAmount: event.target.value,
+      });
+      */
+  };
+
+  const dateChangeHandler = (event) => {
+    setEnteredDate(event.target.value);
+    /*setUserInput({
+        ...userInput,
+        enteredDate: event.target.value,
+      });
+      */
+    /*setUserInput((prevState) => {
+        return {
+          ...prevState,
+          enteredTitle: event.target.value,
+        };
+      });
+      */
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredAmount,
+      date: new Date(enteredDate),
+    };
+    console.log(expenseData);
   };
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense_controls">
         <div className="new-expense_control">
           <label>Title</label>
@@ -20,11 +72,21 @@ const ExpenseForm = (props) => {
         </div>
         <div className="new-expense_control">
           <label>amount</label>
-          <input type="number" min="0.01" step="0.01" />
+          <input
+            type="number"
+            min="0.01"
+            step="0.01"
+            onChange={amountChangeHandler}
+          />
         </div>
         <div className="new-expense_control">
           <label>Date</label>
-          <input type="date" min="2023-01-01" max="2024-12-31" />
+          <input
+            type="date"
+            min="2023-01-01"
+            max="2024-12-31"
+            onChange={dateChangeHandler}
+          />
         </div>
       </div>
       <div className="new-expense_actions">
